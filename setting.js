23a711fcb8da067919e84d9273111074b4a75af2
DEW-MD-LK <hansadewminalk@gmail.com>
2025-03-15 21:42:41 +0530

    Update setting.js

--- setting.js
+++ setting.js
@@ -14,5 +14,15 @@ AUTO_REACT: process.env.AUTO_REACT || "true",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
-LANG: process.env.LANG || "SI"
+LANG: process.env.LANG || "SI",
+AUTO_BIO: process.env.AUTO_BIO || "true",
+AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
+AUTO_REACT: process.env.AUTO_REACT || "true",
+AUTO_TYPING: process.env.AUTO_REACT || "true",
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
+READ_CMD: process.env.READ_CMD || "true",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+ANTI_BAD: process.env.ANTI_BAD || "true",
+ANTI_LINK: process.env.ANTI_LINK || "true"
 };
